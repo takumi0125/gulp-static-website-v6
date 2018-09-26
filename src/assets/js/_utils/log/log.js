0bca4af25597342fde93385921158cb6fd8d640d
Takumi Hasegawa <takumi@tkmh.me>
2018-09-27 00:11:55 +0900

    modified

--- src/assets/js/_utils/log/log.js
+++ src/assets/js/_utils/log/log.js
@@ -1,15 +1,5 @@
 // console.log wrapper
-const _ENV = require('../../_env.js');
-
-function productionlog() {
-  return
-}
-
 export default (function() {
-  if (location.hostname == 'bodystyling.rizap.jp') {
-    return productionlog
-  }
-
   if (window.console != null) {
     if (window.console.log.bind != null) {
       return window.console.log.bind(window.console);
